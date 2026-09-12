7c289875c2822909a10695d7ccfb7fc790f8ebde
yash <yashsrane1818@gmail.com>
2026-09-12 23:54:20 +0530

    Update resume and portfolio data

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -13,7 +13,7 @@ export const contact = {
   phone: "+91 70195 75272",
   linkedin: "https://www.linkedin.com/in/yash-rane701988378",
   github: "https://github.com/yashrane1810",
-  resumeFile: "/resume_YR.pdf",
+  resumeFile: "/Yash_Rane_Resume.pdf",
 };
 
 export const experience = [
